80e618fa3b450e3dbb5473424461c0735f30c122
Lukasz Motowidlo <lmotowid@gmail.com>
2020-06-22 19:12:35 +0200

    .

---
+++ PhotoMap.MobileApp/PhotoMap.MobileApp/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using PhotoMap.MobileApp.Services;
+using PhotoMap.MobileApp.Views;
+
+namespace PhotoMap.MobileApp
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
