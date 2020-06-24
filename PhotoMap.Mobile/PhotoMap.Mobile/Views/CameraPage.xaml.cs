2ad71ea94a28bfdd1c3bea729c99436af9f0c781
Lukasz Motowidlo <lmotowid@gmail.com>
2020-06-24 11:22:16 +0200

    Działa nawigacja i kamera.

--- PhotoMap.Mobile/PhotoMap.Mobile/Views/CameraPage.xaml.cs
+++ PhotoMap.Mobile/PhotoMap.Mobile/Views/CameraPage.xaml.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+
 
 namespace PhotoMap.Mobile.Views
 {
@@ -18,6 +16,33 @@ namespace PhotoMap.Mobile.Views
         }
         private async void CameraButton_Clicked(object sender, EventArgs e)
         {
+            try
+            {
+                var request = new GeolocationRequest(GeolocationAccuracy.Medium);
+                var location = await Geolocation.GetLocationAsync(request);
+
+                if (location != null)
+                {
+                    Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                // Handle not enabled on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                // Unable to get location
+            }
+
             var photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions() { });
 
             if (photo != null)
